29fe54051976cce90b375581c9a374ef79f2b0d4
Paul <paul.landgraf009@gmail.com>
2026-01-04 23:49:18 +0100

    implemented dashboard template from shadcn/vue blocks

---
+++ frontend/src/pages/Dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+import SiteHeader from '@/components/SiteHeader.vue'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar variant="inset" />
+    <SidebarInset>
+      <SiteHeader />
+    </SidebarInset>
+  </SidebarProvider>
+</template>

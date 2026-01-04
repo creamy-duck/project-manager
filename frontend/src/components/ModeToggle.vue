4c7c256cae3908fd363a7374b6d21ba84eaeb211
Paul <paul.landgraf009@gmail.com>
2026-01-05 00:11:23 +0100

    implemented darkmode

---
+++ frontend/src/components/ModeToggle.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+
+import { IconMoon, IconSun } from '@tabler/icons-vue'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+// Pass { disableTransition: false } to enable transitions
+const mode = useColorMode()
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <!--TODO: REDO DESIGN-->
+      <Button variant="outline">
+        Appearance
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
